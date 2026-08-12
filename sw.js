f927e801e3125a732171a430c8f4acdb8c36cb53
BastienZaff <bastfoot21@gmail.com>
2026-08-12 17:35:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Dévoreur Explorer — fonctionnement hors connexion.
    Change le numéro de version ci-dessous après chaque modification. */
 
-var VERSION = 'tables-v85';
+var VERSION = 'tables-v86';
 
 var COQUILLE = [
   './',
